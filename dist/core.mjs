e76c54408de8c4dfe370e96ac12f79e1fff2f6b5
Egehan KAHRAMAN <vastseaoffical0@outlook.com>
2026-09-18 00:49:28 +0300

    chore: add gitignore and untrack build artifacts

--- dist/core.mjs
+++
@@ -1,8 +0,0 @@
-import {
-  WikiEngine
-} from "./chunk-OOIBFQPW.mjs";
-import "./chunk-2W46FCB5.mjs";
-import "./chunk-U6QECY37.mjs";
-export {
-  WikiEngine
-};
--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { DatabaseStats, LinkStatus, ParseWikiOptions, ParsedWikiDocument, ScriptContext, WikiCandidate, WikiCandidateEntity, WikiDatabaseRedLink, WikiHeading, WikiInfobox, WikiInfoboxField, WikiMention, WikiMetadataItem, WikiPageFrontmatter, WikiPageSummary } from './types.js';
-export { extractFrontmatter, parseMetadataDocument, parseWikiContent, slugify } from './parser.js';
-export { executeBlockDirectives, parseDirective, replaceInlineDirectives } from './scriptEngine.js';
-export { LoadWikiOptions, LoadedWikiPage, WikiEngine } from './core.js';
